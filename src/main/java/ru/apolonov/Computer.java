bcd797b6a0b4a3456a7e34432a4b2cc2bd11426c
vasvap <vasvap@gmail.com>
2021-12-01 21:44:18 +0300

    method ramAdd added

--- src/main/java/ru/apolonov/Computer.java
+++ src/main/java/ru/apolonov/Computer.java
@@ -9,8 +9,12 @@ public class Computer {
     int ssd;
 
     public void printConfig() {
-        System.out.println("Конфигурация компьютера: процессор - " + cpu + ", частота - " + cpuSpeed + " ГГц, " + "объем оперативной памяти - " + ram + " ГБ, " +
-                "материнская плата - " + motherBoard + ", жесткий диск - " + ssd + " Гб.");
+        System.out.println("Конфигурация компьютера:" +
+                "процессор - " + cpu + "," +
+                "частота - " + cpuSpeed + " ГГц, " +
+                "объем оперативной памяти - " + ram + " ГБ, " +
+                "материнская плата - " + motherBoard +
+                ", жесткий диск - " + ssd + " Гб.");
     }
 
     public void ramAdd() {
